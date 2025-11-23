89c132b3928e726cfd56e33858dbc8c814265e90
Collins <jimc93005@gmail.com>
2025-11-23 16:57:41 +0200

    Fix database config for Render

--- secondary_school/settings.py
+++ secondary_school/settings.py
@@ -82,13 +82,18 @@ WSGI_APPLICATION = 'secondary_school.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR}/db.sqlite3",
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
         conn_max_age=600,
         ssl_require=False
     )
 }
+
+
 
 
 # Password validation
