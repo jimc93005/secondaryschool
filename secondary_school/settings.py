a92a9706eb122e6f756f527a56e1abff15a6d3e4
Collins <jimc93005@gmail.com>
2025-11-23 17:31:52 +0200

    Configure database for Render deployment

--- secondary_school/settings.py
+++ secondary_school/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
         conn_max_age=600,
-        ssl_require=False
+        ssl_require=True
     )
 }
 
